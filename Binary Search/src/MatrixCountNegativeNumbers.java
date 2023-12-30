553376ef1071cfb073076a99906a6fe3522dad42
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2023-12-30 17:29:27 +0530

    learned basic bubble sort in java

---
+++ Binary Search/src/MatrixCountNegativeNumbers.java
@@ -0,0 +1,27 @@
+public class MatrixCountNegativeNumbers {
+    public static void main(String[] args) {
+        int[][] grid = {
+                {4, 3, 2, -1},
+                {3, 2, 1, -1},
+                {1, 1, -1, -2},
+                {-1, -1, -2, -3}
+        };
+        System.out.println(countNegatives(grid));
+    }
+
+    public static int countNegatives(int[][] grid) {
+        int row = 0;
+        int col = grid.length - 1;
+        int count = 0;
+
+        while (row < grid.length && col >= 0) {
+            if (grid[row][col] < 0) {
+                count += grid.length - row;
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return count;
+    }
+}
