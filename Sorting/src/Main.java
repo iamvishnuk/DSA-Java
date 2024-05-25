34dea18f483ba2b20889595af85eccd299e30d49
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2024-05-25 09:59:32 +0530

    sorting add

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] arr = { 5, 4, 3, 2, 1};
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -35,6 +35,18 @@ public class Main {
             int last = arr.length - i - 1;
             int maxIndex = getMaxIndex(arr, 0, last);
             swap(arr, maxIndex, last);
+        }
+    }
+
+    static  void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0 ; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
         }
     }
 
